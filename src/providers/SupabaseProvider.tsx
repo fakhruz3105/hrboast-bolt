22d0c862182731a9c53de6349da961735f7b3e91
Razi Mutussin <fakhruz3105@gmail.com>
2025-04-08 06:38:55 +0800

    remove unused import

--- src/providers/SupabaseProvider.tsx
+++ src/providers/SupabaseProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, ReactNode, useState, useEffect, useMemo } from 'react';
+import React, { createContext, useContext, ReactNode, useState, useEffect } from 'react';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 // Define the context type
